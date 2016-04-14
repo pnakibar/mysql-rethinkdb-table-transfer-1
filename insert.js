4712bcb275799e56d967cd3575134aabab4ab310
clayton@xdevel.com.br <clayton@xdevel.com.br>
2016-04-14 08:58:09 -0300

    adicionado lint alterado template para se adequar ao novo modelo feito carga de imagens dentro da materia

--- insert.js
+++ insert.js
@@ -4,7 +4,7 @@
 
 (function() {
 
-  "use strict";
+  'use strict';
   const _ = require( 'lodash' );
   const fs = require( 'fs' );
   const ejs2html = require( './services/ejs2html' );
@@ -18,7 +18,7 @@
   shortid.characters( '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$!' );
 
   // args
-  const INTEGRATION_NAME = process.argv[ 2 ];
+  const INTEGRATION_NAME = process.argv[2];
   const SQL_FILE = 'sqls/' + INTEGRATION_NAME + '.sql';
   const RETHINK_TABLE = process.argv[3];
   const LOWER_BOUND = process.argv[4] ? process.argv[4] : null;
@@ -30,7 +30,7 @@
         // resposta do cxense
         console.log( 'inserido no cxense: ' + row.old_materia_id );
         console.log( 'resposta cxense: ' );
-        console.log( response )
+        console.log( response );
 
       } );
   };
@@ -58,54 +58,61 @@
       return xret;
     };
 
-    let str = row.old_materia_path;
+    let preencheImagens = ( imagem ) => {
+      let xret = {};
+      xret.url = imagem.link;
+      xret.default = ( imagem.is_default === 1 );
+      xret.caption = null;
+      xret.width = imagem.width;
+      xret.height = imagem.height;
+
+      return xret;
+    };
+
+    let str = row.old_materia_path.replace( '/_conteudo', '' );
 
     // tratando o endereço antigo.
-    let rexp_path = str.match( /(^[^_]*_conteudo)|(?=[A-Za-z0-9.-]+\.[A-Za-z0-9.-]{3,4}$).+/g );
-    let rexp_file = _.tail( _.head( str.match( /((?=[A-Za-z.-]+\.[A-Za-z0-9.-]{3,4}$).+)/g ) ) );
+    let rexp_path = str.match( /(?=[A-Za-z0-9.-]+\.[A-Za-z0-9.-]{3,4}$).+/g );
+    let rexp_file =  _.head( _.tail( _.last( rexp_path ).match( /(.+?)(\.[^.]*$|$)/i ) ) );
+    let rexp_suffix = _.last( _.last( rexp_path ).match( /(.+?)(\.[^.]*$|$)/i ) );
 
-    let taxonomia = "";
-    rexp_path.forEach( ( el ) => {
-      taxonomia = str.replace( el, '' );
-    } );
-
-    let file = rexp_file + '-' + data.friendlyId;
+    let taxonomia = str.replace( rexp_file + rexp_suffix, '' );
+    let file = rexp_file + '-' + data.friendlyId + rexp_suffix;
 
     data.materia = {
-      "taxonomia": taxonomia,
-      "file":      file,
-      "autor":     row.autores.map( d=> preencheAutores( d ) ),
-      "tag":       row.old_materia_palavra ? row.old_materia_palavra.split( ',' ) : [],
-      "sessao":    "",
-      "local":     "",
-      "imagem":    {
-        "url":     "", // aguardar email de deuclerio da analise do midia
-        "caption": row.old_materia_midia
-      },
-      "materia":   {
-        "id":                   row.old_materia_id,
-        "dataInclusao":         row.old_materia_data_inclusao,
-        "dataPublicacao":       row.old_materia_data_publicacao,
-        "assunto":              row.old_materia_assunto,
-        "titulo":               row.old_materia_titulo,
-        "conteudo":             {
-          "subTitulo": row.old_materia_chape, "newsContent": row.old_materia_ds
+      'taxonomia': taxonomia,
+      'file':      file,
+      'autor':     row.autores.map( ( d ) => preencheAutores( d ) ),
+      'imagens':   row.imagens.map( ( d ) => preencheImagens( d ) ),
+      'tag':       row.old_materia_palavra ? row.old_materia_palavra.split( ',' ) : [],
+      //'imagem':    {
+      //  'url':     '', // aguardar email de deuclerio da analise do midia
+      //  'caption': row.old_materia_midia
+      //},
+      'materia':   {
+        'id':                   row.old_materia_id,
+        'dataInclusao':         row.old_materia_data_inclusao,
+        'dataPublicacao':       row.old_materia_data_publicacao,
+        'assunto':              row.old_materia_assunto,
+        'titulo':               row.old_materia_titulo,
+        'conteudo':             {
+          'subTitulo': row.old_materia_chape, 'newsContent': row.old_materia_ds
         },
-        "chamada":              row.old_materia_chape,
-        "endEletronico":        row.old_materia_path,
-        "endEletronicoCompart": row.old_materia_path_encurtado,
-        "impresso":             {
-          "edicao": row.old_jornal_cd + ' ' + row.old_jornal_ds, "ehPaga": false
+        'chamada':              row.old_materia_chape,
+        'endEletronico':        row.old_materia_path,
+        'endEletronicoCompart': row.old_materia_path_encurtado,
+        'impresso':             {
+          'edicao': row.old_jornal_cd + ' ' + row.old_jornal_ds, 'ehPaga': false
         },
-        "podeComentar":         true,
-        "geolocalizacao":       {
-          "latitude": 0, "longitude": 0
+        'podeComentar':         true,
+        'geolocalizacao':       {
+          'latitude': 0, 'longitude': 0
         },
         tipo:                   row.old_materia_assunto
       },
-      "fonte":     {
-        "nome":          row.old_noticia_nm_fonte ? row.old_noticia_nm_fonte : null,
-        "endEletronico": row.old_noticia_ds_fonte_url ? row.old_noticia_ds_fonte_url : null
+      'fonte':     {
+        'nome':          row.old_noticia_nm_fonte ? row.old_noticia_nm_fonte : null,
+        'endEletronico': row.old_noticia_ds_fonte_url ? row.old_noticia_ds_fonte_url : null
       }
 
     };
@@ -138,15 +145,15 @@
   let callMaxValue = ( conn ) => {
     console.info( 'parte 1 - obtendo o old_materia_cd' );
     return rUtils.r.table( RETHINK_TABLE )
-      .filter( {sourceType: INTEGRATION_NAME} )
+      .filter( { sourceType: INTEGRATION_NAME } )
       .max( 'sourceId' )
       .run( conn )
       .catch( ( err ) => {
         if ( err ) {
           console.warn( 'rethinkdb está vazio!' );
-          return {sourceId: -1}
+          return { sourceId: -1 };
         }
-      } )
+      } );
   };
 
   let assemblySQL = ( maxValue, lowerBound, upperBound, sqlpath ) => {
@@ -173,10 +180,33 @@
   // ler arquivo sql
   let getAutores = ( cd_matia, mysqlconn ) => {
     console.info( 'obtendo autor para a materia ' + cd_matia );
-    return mysqlconn.select( '*' )
+
+
+    return mysqlconn
+      .select( '*' )
       .from( 'autmt' )
       .innerJoin( 'autor', 'autmt.cd_autor', 'autor.cd_autor' )
-      .where( 'cd_matia', '=', cd_matia )
+      .where( 'cd_matia', '=', cd_matia );
+  };
+
+  // ler arquivo sql
+  let getImagens = ( cd_matia, mysqlconn ) => {
+    console.info( 'obtendo midias para a materia ' + cd_matia );
+
+    let select_str = `ds_midia_credi as credito,
+	     ds_midia_link as link ,
+       nm_midia_inter_thumb1 as thumb1,
+       nm_midia_inter_thumb2 as thumb2,
+       nm_midia_inter_thumb3 as thumb3,
+       ds_midma_legen as caption,
+       id_midma_princ as is_default,
+       cd_midia_h as height,
+       cd_midia_w as width`;
+
+    return mysqlconn.select( select_str )
+      .from( 'midma' )
+      .innerJoin( 'midia', 'midma.cd_midia', 'midia.cd_midia' )
+      .where( 'midma.cd_matia', '=', cd_matia );
   };
 
   let insere_autores = ( row, mysqlconn ) => {
@@ -195,26 +225,44 @@
 
   };
 
+
+  let insere_imagens = ( row, mysqlconn ) => {
+
+    let xret = row;
+
+    return getImagens( xret.old_materia_cd, mysqlconn )
+      .then( ( imagens ) => {
+        // adiciona coluna autores no json
+        console.log(xret);
+        console.info( 'obtido numero de imagens da materia ' + xret.old_materia_cd + ':' + imagens.length );
+        xret.imagens = imagens;
+        return xret;
+      } ).catch( (err) => {
+        console.error( 'erro obtendo imagens da materia = ', xret.old_materia_cd );
+      } );
+
+  };
+
   rUtils.getConfig( 'gazetaonline_filme' )
     .then( ( config ) => {
       const knex = require( 'knex' )( {
         client: 'mysql', connection: {
-          host:     config.db.dbhost,
-          user:     config.db.dbuser,
+          host: config.db.dbhost,
+          user: config.db.dbuser,
           password: config.db.dbpass,
           database: config.db.dbname,
-          charset:  'latin1' //config.charset
+          charset: 'latin1' //config.charset
         }
       } );
 
       return knex;
     } )
-    .then( ( knex ) => Promise.all( [knex, rUtils.getConnection] ) )
+    .then( ( knex ) => Promise.all( [ knex, rUtils.getConnection ] ) )
     //todo testar o fluxo
     .then( ( result ) => {
       // implementar range para simular stream com _.range([start=0], end, [step=1])
-
-      let knex = result[0], conn = result[1];
+      let knex = result[0];
+      let conn = result[1];
       let dados = callMaxValue( conn )
         .then( ( maxValue ) => getMySQLdata( assemblySQL( maxValue, LOWER_BOUND, UPPER_BOUND, SQL_FILE ), knex ) )
         .then( ( response ) => _.head( response ) );
@@ -225,14 +273,15 @@
 
         linhas.forEach( ( row ) => {
           let promise = insere_autores( row, knex )
-          //todo enfileirar a operacao de gravar no cxense e processar o html da materia aqui
-            .then( ( row ) => {
-              let wdata = transform( row );
-              return ejs2html.ejs2html( 'template.ejs', __dirname + '/model', wdata.file, __dirname + wdata.taxonomia, wdata )
-                .then( ( result ) => wdata );
+            //todo enfileirar a operacao de gravar no cxense e processar o html da materia aqui
+            .then( ( row_with_authors ) => insere_imagens( row_with_authors , knex ) )
+            .then( ( row_with_authors_images ) => {
+              let wdata = transform( row_with_authors_images );
+              return ejs2html.ejs2html( 'template.ejs', __dirname + '/model', wdata.materia.file, __dirname + '/out/' + wdata.materia.taxonomia, { noticia : wdata.materia } )
+                .then( () => wdata );
             } )
             .then( ( wdata ) => {
-              return writeRowRethink( wdata, conn )
+              return writeRowRethink( wdata, conn );
             } )
             .catch( ( err ) => console.error( err.message ) );
 
@@ -244,12 +293,12 @@
         Promise.all( promises ).then( () => {
           console.info( 'processo finalizado com sucesso' );
           process.exit( 0 );
-        } )
+        } );
 
       } ).catch( ( err ) => {
         console.log( err.message );
         process.exit( -1 );
-      } )
+      } );
 
     } )
     .catch( ( err ) => {
--- model/materia.js
+++ model/materia.js
@@ -1,83 +1,53 @@
 /**
  * definição de todos os modelos de modelagem
  */
-(function () {
-    'use strict';
-    let autor = {
-        "nome": "",
-        "email": "",
-        "facebook": "",
-        "instagram": "",
-        "twitter": "",
-        "endEletronicoPessoal": ""
-    };
+(function() {
+  'use strict';
+  let autor = {
+    'nome':                 '',
+    'email':                '',
+    'facebook':             '',
+    'instagram':            '',
+    'twitter':              '',
+    'endEletronicoPessoal': ''
+  };
 
+  let materia = {
+    'friendlyId':  '', 'autor': [], 'tema': {
+      'nome': ''
+    }, 'tag':      [], 'sessao': '', 'local': '', 'imagens': [ {
+      'url': '', 'default': '', 'caption': '', 'width': '', 'height': ''
+    } ], 'materia': {
+      'id':                   0,
+      'dataInclusao':         '',
+      'dataPublicacao':       '',
+      'assunto':              '',
+      'titulo':               '',
+      'conteudo':             {
+        'subTitulo': '', 'newsContent': ''
+      },
+      'chamada':              '',
+      'endEletronico':        '',
+      'endEletronicoCompart': '',
+      'impresso':             {
+        'edicao': '', 'ehPaga': false
+      },
+      'podeComentar':         true,
+      'geolocalizacao':       {
+        'latitude': 0, 'longitude': 0
+      },
+      'dataValidade':         ''
+    }, tipo:       '', 'fonte': {
+      'nome': '', 'endEletronico': ''
+    }
+  };
 
+  let persistData = {
+    sourceType: '', sourceData: {}, sourceId: '', materia: materia, cxResponse: {}
+  };
 
-    let materia = {
-        "friendlyId" : "",
-        "autor": [],
-        "tema": {
-            "nome": ""
-        },
-        "tag": [],
-        "sessao": "",
-        "local": "",
-        "imagens": [{
-            "url": "",
-            "default": "",
-            "caption": "",
-            "width" : "",
-            "height": ""
-        }],
-        "materia": {
-            "id": 0,
-            "dataInclusao": "",
-            "dataPublicacao": "",
-            "assunto": "",
-            "titulo": "",
-            "conteudo": {
-                "subTitulo": "",
-                "newsContent": ""
-            },
-            "chamada": "",
-            "endEletronico": "",
-            "endEletronicoCompart": "",
-            "impresso": {
-                "edicao": "",
-                "ehPaga": false
-            },
-            "podeComentar": true,
-            "geolocalizacao": {
-                "latitude": 0,
-                "longitude": 0
-            },
-            "dataValidade": ""
-        },
-        tipo: "",
-        "fonte": {
-            "nome": "",
-            "endEletronico": ""
-        }
-    };
-
-
-    let persistData = {
-        sourceType : "" ,
-        sourceData : {} ,
-        sourceId : "",
-        materia : materia,
-        cxResponse : {}
-    };
-
-
-
-
-    module.exports = {
-        materia : materia,
-        autor : autor,
-        persistData : persistData
-    };
-
+  module.exports = {
+    materia: materia, autor: autor, persistData: persistData
+  };
 
 }());
