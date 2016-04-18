fc589173e4287af30d95399d7ba6165d062e5244
pn <pedro.nakibar@xdevel.com.br>
2016-04-18 16:13:34 -0300

    adicionada tag <i>

--- utils/removeUglyTags.js
+++ utils/removeUglyTags.js
@@ -1,5 +1,6 @@
 module.exports = (text) => {
   'use strict'
-  let regex = /<(?!br|b|spam|a|div|iframe|strong|em|p|\/a|\/div|\/p|\/iframe|\/b|\/strong|\/em|\/spam).*?>/g
+  // remove tags que NÃO estão espeficadas abaixo
+  let regex = /<(?!br|b|spam|a|div|iframe|strong|em|p|\/a|\/div|\/p|\/iframe|\/b|\/strong|\/em|\/spam|i|\/i).*?>/g
   return text.replace(regex, '')
 }
